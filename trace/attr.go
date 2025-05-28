640efbee7edfc92a3b2e89285334438088e398dc
Joe Schafer <joe@jschaf.com>
2025-05-27 17:58:20 -0700

    trace: add int64, float64, string slice attrs

--- trace/attr.go
+++ trace/attr.go
@@ -11,12 +11,6 @@ func Bool(key string, value bool) Attr {
 	return Attr{key, boolValue(value)}
 }
 
-// Bools returns an Attr for a bool slice.
-// Only records the first 56 elements, dropping subsequent elements.
-func Bools(key string, value []bool) Attr {
-	return Attr{key, boolsValue(value)}
-}
-
 // Float64 returns an Attr for a floating-point number.
 func Float64(key string, value float64) Attr {
 	return Attr{key, float64Value(value)}
@@ -35,6 +29,32 @@ func Int(key string, value int) Attr {
 // String returns an Attr for a string value.
 func String(key, value string) Attr {
 	return Attr{key, stringValue(value)}
+}
+
+// Bools returns an Attr for a bool slice.
+// Only records the first 56 elements, dropping subsequent elements.
+func Bools(key string, value []bool) Attr {
+	return Attr{key, boolsValue(value)}
+}
+
+// Float64s returns an Attr for a float64 slice.
+func Float64s(key string, value []float64) Attr {
+	return Attr{key, float64sValue(value)}
+}
+
+// Ints returns an Attr for an int slice.
+func Ints(key string, value []int) Attr {
+	return Attr{key, intsValue(value)}
+}
+
+// Int64s returns an Attr for an int64 slice.
+func Int64s(key string, value []int64) Attr {
+	return Attr{key, int64sValue(value)}
+}
+
+// Strings returns an Attr for a string slice.
+func Strings(key string, value []string) Attr {
+	return Attr{key, stringsValue(value)}
 }
 
 func (a Attr) String() string {
