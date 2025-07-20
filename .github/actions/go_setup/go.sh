52aeee29004984b72cf10bb7e0ec6da00a409a0e
Joe Schafer <joe.schafer@delta46.us>
2025-07-19 23:44:12 -0700

    .github: use custom go action (#10)
    The official setup-go action spends the majority of time caching the go binary
    and attempting to cache the go.sum entry.

---
+++ .github/actions/go_setup/go.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo '::group::Download Go'
+
+# Read Go version from go.mod.
+version=$(grep '^go ' "${GITHUB_WORKSPACE}/go.mod" | awk '{print $2}')
+
+# Detect platform and arch.
+platform=$(uname -s | tr '[:upper:]' '[:lower:]')
+case "$(uname -m)" in
+    x86_64) arch="amd64" ;;
+    aarch64|arm64) arch="arm64" ;;
+    *) arch="amd64" ;;  # default fallback
+esac
+echo "Go version: $version, platform: $platform, arch: $arch"
+
+# Add to path.
+goroot="$RUNNER_TOOL_CACHE/go/$version/$arch"
+mkdir -p "$goroot"
+echo "$goroot/bin" >> "$GITHUB_PATH"
+
+# Download.
+archive_url="https://go.dev/dl/go${version}.${platform}-${arch}.tar.gz"
+echo "Downloading from: $archive_url"
+curl --fail --silent --show-error --location --write-out "%{stderr}Downloaded in %{time_total} seconds\n" "$archive_url" \
+  | tar -xz --strip-components=1 -C "$goroot"
+
+"$goroot/bin/go" version
+echo '::endgroup::'
+
