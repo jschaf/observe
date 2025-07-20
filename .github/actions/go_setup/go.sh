b7f80f67e80b55601972d44464a969b12f602cea
Joe Schafer <joe@jschaf.com>
2025-07-19 22:58:27 -0700

    simplify

--- .github/actions/go_setup/go.sh
+++ .github/actions/go_setup/go.sh
@@ -2,13 +2,9 @@
 set -euo pipefail
 
 echo '::group::Download Go'
-goroot="./go"
-mkdir -p "$goroot"
-echo "$goroot/bin" >> "$GITHUB_PATH"
 
 # Read Go version from go.mod.
 version=$(grep '^go ' "${GITHUB_WORKSPACE}/go.mod" | awk '{print $2}')
-echo "Using Go version: $version"
 
 # Detect platform and arch.
 platform=$(uname -s | tr '[:upper:]' '[:lower:]')
@@ -17,6 +13,11 @@ case "$(uname -m)" in
     aarch64|arm64) arch="arm64" ;;
     *) arch="amd64" ;;  # default fallback
 esac
+echo "Go version: $version, platform: $platform, arch: $arch"
+
+goroot="$RUNNER_TOOL_CACHE/go/$version/$arch"
+mkdir -p "$goroot"
+echo "$goroot/bin" >> "$GITHUB_PATH"
 
 # Download.
 archive_url="https://go.dev/dl/go${version}.${platform}-${arch}.tar.gz"
@@ -24,7 +25,6 @@ echo "Downloading from: $archive_url"
 curl --fail --silent --show-error --location --write-out "%{stderr}Downloaded in %{time_total} seconds\n" "$archive_url" \
   | tar -xz --strip-components=1 -C "$goroot"
 
-export PATH="$PATH:$goroot"
-go version
+"$goroot/bin/go" version
 echo '::endgroup::'
 
